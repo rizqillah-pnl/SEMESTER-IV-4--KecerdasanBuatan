135194d0c6f0baceabe1537b90ef12c9fb0cfa3f
Rizqillah <71523218+rizqillah-pnl@users.noreply.github.com>
2021-04-28 17:55:06 +0700

    menambah file Prak 1

---
+++ Prak 1 - BFS & DFS/Program/bfs.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+
+
+class Graph:
+    # constructor
+    def __init__(self):
+
+        # default dictionary to store graph
+        self.graph = defaultdict(list)
+
+    # fungsi menambah edge di graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    # fungsi utk print BFS graph
+    def BFS(self, s):
+        visited = [False] * (max(self.graph) + 1)
+
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Berikut adalah Breadth First Traversal" " (starting from vertex 2)")
+g.BFS(2)
